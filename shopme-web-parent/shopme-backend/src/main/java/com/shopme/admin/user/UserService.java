5037e98b148255f7574c098e8cbea36b426345ba
Shiyan Chen <ch592127418@gmail.com>
2022-07-10 23:05:58 -0700

    add user create page

--- shopme-web-parent/shopme-backend/src/main/java/com/shopme/admin/user/UserService.java
+++ shopme-web-parent/shopme-backend/src/main/java/com/shopme/admin/user/UserService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.shopme.common.entity.Role;
 import com.shopme.common.entity.User;
 
 
@@ -12,10 +13,21 @@ import com.shopme.common.entity.User;
 public class UserService {
 	
 	@Autowired
-	private UserRepository repo;
+	private UserRepository userRepo;
+	
+	@Autowired
+	private RoleRepository roleRepo;
 	
 	public List<User> listAll() {
-		return (List<User>) repo.findAll();
+		return (List<User>) userRepo.findAll();
+	}
+	
+	public List<Role> listRoles() {
+		return (List<Role>) roleRepo.findAll();	
+	}
+	
+	public void save(User user) {
+		userRepo.save(user);
 	}
 
 }
