415b8edad3c9870ed049c1703b5c7db92f49166f
Shiyan Chen <ch592127418@gmail.com>
2022-07-17 11:12:34 -0700

    encode user password

--- shopme-web-parent/shopme-backend/src/test/java/com/shopme/admin/user/PasswordEncoderTest.java
+++ shopme-web-parent/shopme-backend/src/test/java/com/shopme/admin/user/PasswordEncoderTest.java
@@ -1,2 +1,21 @@
-package com.shopme.admin.user;public class PasswordEncoderTest {
+package com.shopme.admin.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PasswordEncoderTest {
+
+    @Test
+    public void testEncodePassword() {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String rawPassword = "123456789";
+        String encodedPassword = passwordEncoder.encode(rawPassword);
+
+        System.out.println(encodedPassword);
+
+        boolean is_matched = passwordEncoder.matches(rawPassword, encodedPassword);
+        assertTrue(is_matched);
+    }
 }
