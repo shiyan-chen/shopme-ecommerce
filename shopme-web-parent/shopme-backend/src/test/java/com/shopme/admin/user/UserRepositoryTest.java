da2bbcf8218e6f51bf03680e8914d1bfc65e743b
Shiyan Chen <ch592127418@gmail.com>
2022-07-22 22:00:11 -0700

    code delete user function

--- shopme-web-parent/shopme-backend/src/test/java/com/shopme/admin/user/UserRepositoryTest.java
+++ shopme-web-parent/shopme-backend/src/test/java/com/shopme/admin/user/UserRepositoryTest.java
@@ -95,5 +95,13 @@ public class UserRepositoryTest {
 
 		assertThat(user).isNotNull();
 	}
+
+	@Test
+	public void testCountById() {
+		Integer id = 1;
+		Long countById = repo.countById(id);
+
+		assertThat(countById).isNotNull().isGreaterThan(0);
+	}
 	
 }
